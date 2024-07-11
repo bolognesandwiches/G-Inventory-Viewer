7409c83d49c853cbc722e5db27e102890e4e6505
bolognesandwiches <168598554+bolognesandwiches@users.noreply.github.com>
2024-07-11 10:53:02 +0100

    dunno

--- summary/summary.go
+++ summary/summary.go
@@ -1,4 +1,4 @@
-package main
+package summary
 
 import (
 	"fmt"
@@ -13,9 +13,9 @@ func GetInventorySummary(items map[int]inventory.Item) string {
 	totalHC := 0.0
 
 	for _, item := range items {
-		name := GetItemName(item.Class, string(item.Type), item.Props)
+		name := main.GetItemName(item.Class, string(item.Type), item.Props)
 		itemCounts[name]++
-		totalHC += GetHCValue(name)
+		totalHC += main.GetHCValue(name)
 	}
 
 	var summary strings.Builder
@@ -25,7 +25,7 @@ func GetInventorySummary(items map[int]inventory.Item) string {
 	summary.WriteString("------------------\n")
 
 	for name, count := range itemCounts {
-		hcValue := GetHCValue(name)
+		hcValue := main.GetHCValue(name)
 		summary.WriteString(fmt.Sprintf("%s: %d (%.2f HC)\n", name, count, hcValue))
 	}
 
@@ -37,15 +37,15 @@ func GetRoomSummary(objects map[int]room.Object, items map[int]room.Item) string
 	totalHC := 0.0
 
 	for _, obj := range objects {
-		name := GetItemName(obj.Class, "S", "")
+		name := main.GetItemName(obj.Class, "S", "")
 		itemCounts[name]++
-		totalHC += GetHCValue(name)
+		totalHC += main.GetHCValue(name)
 	}
 
 	for _, item := range items {
-		name := GetItemName(item.Class, "I", item.Type)
+		name := main.GetItemName(item.Class, "I", item.Type)
 		itemCounts[name]++
-		totalHC += GetHCValue(name)
+		totalHC += main.GetHCValue(name)
 	}
 
 	var summary strings.Builder
@@ -55,7 +55,7 @@ func GetRoomSummary(objects map[int]room.Object, items map[int]room.Item) string
 	summary.WriteString("------------------\n")
 
 	for name, count := range itemCounts {
-		hcValue := GetHCValue(name)
+		hcValue := main.GetHCValue(name)
 		summary.WriteString(fmt.Sprintf("%s: %d (%.2f HC)\n", name, count, hcValue))
 	}
 
@@ -63,19 +63,19 @@ func GetRoomSummary(objects map[int]room.Object, items map[int]room.Item) string
 }
 
 func GetInventoryItemDetails(item inventory.Item) string {
-	name := GetItemName(item.Class, string(item.Type), item.Props)
+	name := main.GetItemName(item.Class, string(item.Type), item.Props)
 	return fmt.Sprintf("Name: %s\nID: %d\nType: %s\nClass: %s\nProps: %s\n",
 		name, item.ItemId, item.Type, item.Class, item.Props)
 }
 
 func GetRoomItemDetails(item room.Item) string {
-	name := GetItemName(item.Class, "I", item.Type)
+	name := main.GetItemName(item.Class, "I", item.Type)
 	return fmt.Sprintf("Name: %s\nID: %d\nClass: %s\nOwner: %s\nLocation: %s\nType: %s\n",
 		name, item.Id, item.Class, item.Owner, item.Location, item.Type)
 }
 
 func GetRoomObjectDetails(obj room.Object) string {
-	name := GetItemName(obj.Class, "S", "")
+	name := main.GetItemName(obj.Class, "S", "")
 	return fmt.Sprintf("Name: %s\nID: %d\nClass: %s\nPosition: (%d, %d, %.2f)\nSize: %dx%d\nDirection: %d\n",
 		name, obj.Id, obj.Class, obj.X, obj.Y, obj.Z, obj.Width, obj.Height, obj.Direction)
 }
